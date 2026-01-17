c1192f5cd17cce3b15aed7a799220d5bd6ab5ffd
wahyufadil1140@gmail.com <wahyufadil1140@gmail.com>
2026-01-18 01:22:02 +0700

    feat: fix error login & manage product

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const PORT = 3000;
 
 // ===== MIDDLEWARE =====
 app.use(express.json());
+app.use(express.urlencoded({ extended: true })); // ⬅️ WAJIB TAMBAH
 app.use(express.static(path.join(__dirname, "public")));
 
 // ===== DATABASE =====
@@ -14,7 +15,7 @@ const db = new sqlite3.Database("./db.sqlite");
 
 // ===== INIT TABLE =====
 db.serialize(() => {
-  db.run(`
+    db.run(`
     CREATE TABLE IF NOT EXISTS users (
       id INTEGER PRIMARY KEY AUTOINCREMENT,
       username TEXT UNIQUE,
@@ -22,7 +23,7 @@ db.serialize(() => {
     )
   `);
 
-  db.run(`
+    db.run(`
     CREATE TABLE IF NOT EXISTS products (
       id INTEGER PRIMARY KEY AUTOINCREMENT,
       name TEXT,
@@ -32,7 +33,7 @@ db.serialize(() => {
     )
   `);
 
-  db.run(`
+    db.run(`
     CREATE TABLE IF NOT EXISTS orders (
       id INTEGER PRIMARY KEY AUTOINCREMENT,
       user_id INTEGER,
@@ -44,81 +45,85 @@ db.serialize(() => {
     )
   `);
 
-  // seed product
-  db.get("SELECT COUNT(*) as c FROM products", (err, row) => {
-    if (row.c === 0) {
-      db.run(`
+    // seed product
+    db.get("SELECT COUNT(*) as c FROM products", (err, row) => {
+        if (row.c === 0) {
+            db.run(`
         INSERT INTO products (name,description,price,image) VALUES
-        ('Milo 1kg','Serbuk cokelat bergizi',95000,'milo 1kg.png'),
-        ('Milo Cereal 330gr','Cereal cokelat renyah',45000,'milo cereal.png'),
-        ('Milo Cube 100gr','Cokelat kubus praktis',30000,'milo cube.jpeg')
+        ('Milo 1kg','Serbuk cokelat bergizi',95000,'https://bimg.akulaku.net/goods/spu/48081f5717c74fbeaab251bc73b8d0c57503.jpg'),
+        ('Milo Cereal 330gr','Cereal cokelat renyah',45000,'https://www.nestle-cereals.com/id/sites/g/files/qirczx876/files/styles/1_1_768px_width/public/2022-12/6.png.webp'),
+        ('Milo Cube 100gr','Cokelat kubus praktis',30000,'https://id-test-11.slatic.net/p/5e158931dc52c47b06f5b8583abafc24.jpg')
       `);
-    }
-  });
+        }
+    });
 });
 
 // ===== ROUTES =====
 app.get("/api/products", (req, res) => {
-  db.all("SELECT * FROM products", (err, rows) => {
-    res.json(rows);
-  });
+    db.all("SELECT * FROM products", (err, rows) => {
+        res.json(rows);
+    });
 });
 
 app.post("/api/register", (req, res) => {
-  const { username, password } = req.body;
-  db.run(
-    "INSERT INTO users (username,password) VALUES (?,?)",
-    [username, password],
-    err => {
-      if (err) return res.json({ success: false });
-      res.json({ success: true });
-    }
-  );
+    const { username, password } = req.body;
+    db.run(
+        "INSERT INTO users (username,password) VALUES (?,?)",
+        [username, password],
+        err => {
+            if (err) return res.json({ success: false });
+            res.json({ success: true });
+        }
+    );
 });
 
 app.post("/api/login", (req, res) => {
-  const { username, password } = req.body;
-  db.get(
-    "SELECT * FROM users WHERE username=? AND password=?",
-    [username, password],
-    (err, row) => {
-      res.json({ success: !!row, user: row });
-    }
-  );
+    console.log("BODY LOGIN:", req.body);
+
+    const { username, password } = req.body;
+    db.get(
+        "SELECT * FROM users WHERE username=? AND password=?",
+        [username, password],
+        (err, row) => {
+            console.log("HASIL QUERY:", row);
+            res.json({ success: !!row, user: row });
+        }
+    );
 });
+
 
 // ===== CHECKOUT =====
 app.post("/api/checkout", (req, res) => {
-  console.log("CHECKOUT HIT", req.body);
+    console.log("CHECKOUT HIT", req.body);
 
-  const { user_id, product_id, qty } = req.body;
+    const { user_id, product_id, qty } = req.body;
 
-  db.get(
-    "SELECT * FROM products WHERE id=?",
-    [product_id],
-    (err, product) => {
-      if (!product) {
-        return res.json({ success: false });
-      }
+    db.get(
+        "SELECT * FROM products WHERE id=?",
+        [product_id],
+        (err, product) => {
+            if (!product) {
+                return res.json({ success: false });
+            }
 
-      const total = product.price * qty;
+            const total = product.price * qty;
 
-      db.run(
-        `
+            db.run(
+                `
         INSERT INTO orders 
         (user_id, product_id, qty, total, status, created_at)
         VALUES (?, ?, ?, ?, 'Diproses', datetime('now'))
         `,
-        [user_id, product_id, qty, total],
-        () => res.json({ success: true })
-      );
-    }
-  );
+                [user_id, product_id, qty, total],
+                () => res.json({ success: true })
+            );
+        }
+    );
 });
 
 // ===== ADMIN =====
 app.get("/api/orders", (req, res) => {
-  db.all(`
+    db.all(`
     SELECT orders.id, users.username, products.name, qty, total, status, created_at
     FROM orders
     JOIN users ON users.id = orders.user_id
@@ -127,42 +132,42 @@ app.get("/api/orders", (req, res) => {
 });
 
 app.post("/api/update-order-status", (req, res) => {
-  const { id, status } = req.body;
-  db.run(
-    "UPDATE orders SET status=? WHERE id=?",
-    [status, id],
-    () => res.json({ success: true })
-  );
+    const { id, status } = req.body;
+    db.run(
+        "UPDATE orders SET status=? WHERE id=?",
+        [status, id],
+        () => res.json({ success: true })
+    );
 });
 
 // ===== CRUD PRODUCT =====
 app.post("/api/add-product", (req, res) => {
-  const { name, description, price, image } = req.body;
-  db.run(
-    "INSERT INTO products (name,description,price,image) VALUES (?,?,?,?)",
-    [name, description, price, image],
-    () => res.json({ success: true })
-  );
+    const { name, description, price, image } = req.body;
+    db.run(
+        "INSERT INTO products (name,description,price,image) VALUES (?,?,?,?)",
+        [name, description, price, image],
+        () => res.json({ success: true })
+    );
 });
 
 app.post("/api/update-product", (req, res) => {
-  const { id, name, description, price, image } = req.body;
-  db.run(
-    "UPDATE products SET name=?,description=?,price=?,image=? WHERE id=?",
-    [name, description, price, image, id],
-    () => res.json({ success: true })
-  );
+    const { id, name, description, price, image } = req.body;
+    db.run(
+        "UPDATE products SET name=?,description=?,price=?,image=? WHERE id=?",
+        [name, description, price, image, id],
+        () => res.json({ success: true })
+    );
 });
 
 app.post("/api/delete-product", (req, res) => {
-  db.run(
-    "DELETE FROM products WHERE id=?",
-    [req.body.id],
-    () => res.json({ success: true })
-  );
+    db.run(
+        "DELETE FROM products WHERE id=?",
+        [req.body.id],
+        () => res.json({ success: true })
+    );
 });
 
 // ===== START SERVER =====
 app.listen(PORT, () => {
-  console.log("SERVER RUNNING ON http://localhost:" + PORT);
+    console.log("SERVER RUNNING ON http://localhost:" + PORT);
 });
